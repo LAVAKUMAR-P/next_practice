700f14279264a1773973691f12a888dbb3e9b2b9
Lavakumar <84187714+LAVAKUMAR-P@users.noreply.github.com>
2024-10-22 22:21:05 +0530

    Learned Loading ...

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,20 @@
+"use client";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+
 
 export default function Home() {
+  const navigate = useRouter()
+  const navigaeTO = ()=>{
+    navigate.push("/blog")
+  }
   return (
     <div>
       <h1>Welcome home!</h1>
       <Link href="/blog">Blog</Link>
       <Link href="/products">Products</Link>
       <Link href="/news">News</Link>
+      <button style={{border:"1px solid red"}} onClick={navigaeTO}>Blog</button>
     </div>
   );
 }
