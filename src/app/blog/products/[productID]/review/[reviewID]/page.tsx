9d013ab9c2a65a3f857f618844d3d11a5fd901b9
Lavakumar <84187714+LAVAKUMAR-P@users.noreply.github.com>
2024-10-24 23:42:36 +0530

    Learned about Error boundary in Next js

--- src/app/blog/products/[productID]/review/[reviewID]/page.tsx
+++ src/app/blog/products/[productID]/review/[reviewID]/page.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 interface data {
     params : {
-        productID : string,
+        productID : string|number,
         reviewID: string,
     }
 }
+
 function page({params}:data) {
+  if(params.productID == 1){
+   throw new Error("Failed to fetch")
+  }
   return (
     <div>product id {params.productID} and reviewId {params.reviewID}</div>
   )
