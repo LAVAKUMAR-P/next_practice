56f20395d6bef3760c06b03882966fc15f9d4713
Lavakumar <84187714+LAVAKUMAR-P@users.noreply.github.com>
2024-11-07 22:00:23 +0530

    client side rendering

---
+++ redering/src/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+import { useState } from "react"
+
+function page() {
+    const [count, setCount] = useState(0);
+  return (
+    <div style={{display:"flex",justifyContent:"center",alignItems:"center",cursor:"pointer"}} onClick={()=>{setCount(count+1)}}>page {count}</div>
+  )
+}
+
+export default page
