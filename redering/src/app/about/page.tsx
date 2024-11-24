50ec1e2d341e6569e9e742f3dc1b71573343eb9b
Lavakumar <84187714+LAVAKUMAR-P@users.noreply.github.com>
2024-11-24 19:18:31 +0530

    SSR
    Server side rendering

--- redering/src/app/about/page.tsx
+++ redering/src/app/about/page.tsx
@@ -1,7 +1,7 @@
 export default function AboutPage() {
   return (
     <>
-      <h1>Lava's About page </h1>
+      <h1>Lava's About page {new Date().toLocaleTimeString()}</h1>
     </>
   );
 }
--- redering/src/app/dashboard/page.tsx
+++ redering/src/app/dashboard/page.tsx
@@ -2,11 +2,11 @@
 
 import { useState } from "react"
 
-function page() {
+function Page() {
     const [count, setCount] = useState(0);
   return (
     <div style={{display:"flex",justifyContent:"center",alignItems:"center",cursor:"pointer"}} onClick={()=>{setCount(count+1)}}>page {count}</div>
   )
 }
 
-export default page
+export default Page
