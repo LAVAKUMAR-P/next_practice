210e2a30d8e011b97ac93299f564369a9bc4d088
Lavakumar <84187714+LAVAKUMAR-P@users.noreply.github.com>
2024-11-25 22:50:27 +0530

    Dynamic Rendering

--- redering/src/app/about/page.tsx
+++ redering/src/app/about/page.tsx
@@ -1,4 +1,9 @@
-export default function AboutPage() {
+import { cookies } from "next/headers";
+
+export default async function AboutPage() {
+ const cookieStore = await cookies(); 
+ const theme = cookieStore.get("theme")
+  console.log(theme)
   return (
     <>
       <h1>Lava's About page {new Date().toLocaleTimeString()}</h1>
